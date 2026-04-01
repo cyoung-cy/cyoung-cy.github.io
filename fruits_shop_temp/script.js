fa07d8e7d9d6ac96632e400b5a5904533bcaac68
cyoung <saidayummy@gmail.com>
2026-04-01 15:47:15 +0900

    finsh

--- fruits_shop_temp/script.js
+++ fruits_shop_temp/script.js
@@ -32,15 +32,54 @@ function renderProducts(data, container) {
 /* 
   과일 출력
 */
+
 function filterAndSortFruits() {
   //화면에 다시 출력
-  //renderProducts(?, ?); //1. 과일에 대한 정보, 2. 햇과일 index.html의 id
+  let data = [];
+  for (let i = 0; i < fruits.length; i++) {
+    let id = fruits[i].id;
+    let name = fruits[i].name;
+    let price = fruits[i].price;
+    let img = fruits[i].img;
+    data[i] = { id, name, price, img };
+  }
+
+  //겁색
+  data = fruits.filter((item) => {
+    return item.name.toLowerCase().includes(searchBox.value.toLowerCase());
+  });
+
+  //정렬
+  if (sortSelect.value === "low") {
+    data.sort((a, b) => a.price - b.price);
+  } else if (sortSelect.value === "high") {
+    data.sort((a, b) => b.price - a.price);
+  } else if (sortSelect.value === "name") {
+    data.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+  }
+
+  renderProducts(data, fruitList); //1. 과일에 대한 정보, 2. 햇과일 index.html의 id
 }
 
 // 채소 출력 (3개씩 증가)
 function loadVeggies() {
+  // 채소 출력 (3개씩 증가)
+  veggiePage += 3;
   //화면에 다시 출력
-  //renderProducts(?, ?); //더보기 버튼 누를 때 실행 할 거임
+  let data = [];
+  for (let i = 0; i < veggiePage; i++) {
+    let id = veggies[i].id;
+    let name = veggies[i].name;
+    let price = veggies[i].price;
+    let img = veggies[i].img;
+    data[i] = { id, name, price, img };
+  }
+
+  renderProducts(data, veggieList); //더보기 버튼 누를 때 실행 할 거임
 }
 ////////////////////////////////////////////////////////
 
